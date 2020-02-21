063d748adcaaac0c4a1696e37f94ca9a042ca341
William Barela <william.barela.dev@gmail.com>
2020-02-21 05:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ const findEmployeeByFirstName = (srcArray, firstName) => {
   return srcArray.find(employeeObject => employeeObject.firstName === firstName);
 };
 
-const calculatePayroll = (srcArray) => {
-  return srcArray.reduce((sum, employeeObject) => {
-    return sum + allWagesFor(employeeObject);
-  }, 0);
-};
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor(rec)
+    }, 0)
+}
