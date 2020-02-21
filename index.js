9ee3fc48064519921289344609a5666cb0989e08
William Barela <william.barela.dev@gmail.com>
2020-02-21 04:48:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,5 +62,7 @@ const wagesEarnedOnDate = (employeeObject, date) => {
 
 const allWagesFor = (employeeObject) => {
   const datesWorked = employeeObject.timeInEvents.map(clockIn => clockIn.date);
-  return datesWorked.reduce((total, date) => wagesEarnedOnDate(employeeObject, date));
-}
+  return datesWorked.reduce((firstDate, date) => {
+    wagesEarnedOnDate(employeeObject, firstDate) + wagesEarnedOnDate(employeeObject, date)
+  });
+};
